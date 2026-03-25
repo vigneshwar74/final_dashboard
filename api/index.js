5da219edb82b851333867113c8a0f26ca0352512
vigneshwar74 <vigneshwar74@users.noreply.github.com>
2026-03-25 11:24:45 +0530

    Use single Vercel API function for Hobby plan

--- api/index.js
+++ api/index.js
@@ -1,3 +1,17 @@
+const { URL } = require('url');
 const { createApp } = require('../server/src/app');
 
-module.exports = createApp();
+const app = createApp();
+
+module.exports = (req, res) => {
+  const incomingUrl = new URL(req.url, 'http://localhost');
+  const pathParts = incomingUrl.searchParams.getAll('path').filter(Boolean);
+
+  if (pathParts.length > 0) {
+    incomingUrl.pathname = `/api/${pathParts.join('/')}`.replace(/\/+/g, '/');
+    incomingUrl.searchParams.delete('path');
+    req.url = incomingUrl.pathname + incomingUrl.search;
+  }
+
+  return app(req, res);
+};
